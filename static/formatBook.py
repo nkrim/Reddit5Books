ba38892be8b0c20c7829e9a616c41ac86316ba97
gramseyer <ramseyer@uchicago.edu>
2014-11-22 23:30:40 -0600

    formatting stuff for templates

---
+++ static/formatBook.py
@@ -0,0 +1,17 @@
+def format(filename, output):
+	f=open(filename)
+	lines=f.readlines()
+	result=open(output,"w")
+	i=0
+	for line in lines:
+		if (len(lines)<=1):
+			line="<br> "
+		result.write("<div id=\"line"+str(i)+"\">"+line[:-1]+"</div>\n")
+		i+=1
+
+def formatcomments(comments):
+	string=""
+	i=0
+	for comment in comments:
+		string=string+"comments["+str(i)"]=new Comment("+str(comment["start"])+","+str(comment["end"])+","+comment["subject"]+","+comment["details"]+");\n"
+	return string
