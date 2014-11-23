469ba2285e9688ffbe4086848f2a33bed75503dd
nkrim62 <nkrim62@gmail.com>
2014-11-22 18:41:04 -0600

    Added mongo stuff

--- static/hello.py
+++ static/hello.py
@@ -2,11 +2,15 @@ from flask import Flask
 from flask.ext.pymongo import PyMongo
 
 app = Flask(__name__)
+mongo = PyMongo(app)
 
 @app.route("/<test>")
 def hello(test):
-	print test
-	return "Hello World!"
+	try:
+		mongo.db.add_user(test,"password")
+	except Exception as e:
+		print e
+	return "User created as " + test
 
 if __name__ == "__main__":
 	app.run()
